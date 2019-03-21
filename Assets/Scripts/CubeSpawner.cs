9c637575f9defa789d59dbb1086023df9aa12095
Max Woods <maxmillianrwoods@gmail.com>
2019-03-21 10:44:39 -0700

    Add working project

---
+++ Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private MovingCube cubePrefab;
+    [SerializeField]
+    private MoveDirection moveDirection;
+    private BoxCollider myBox;
+
+    private void start()
+    {
+        myBox = GetComponent<BoxCollider>();
+    }
+
+    public void SpawnCube()
+    {
+        var cube = Instantiate(cubePrefab);
+        
+
+        if (MovingCube.LastCube != null && MovingCube.LastCube.gameObject != GameObject.Find("Start"))
+        {
+            float x = moveDirection == MoveDirection.X ? transform.position.x : MovingCube.LastCube.transform.position.x;
+            float z = moveDirection == MoveDirection.Z ? transform.position.z : MovingCube.LastCube.transform.position.z;
+            cube.transform.position = new Vector3(transform.position.x,
+                MovingCube.LastCube.transform.position.y + cubePrefab.transform.localScale.y,
+                transform.position.z);
+            cube.GetComponent<Rigidbody>();
+        }
+        else
+        {
+            cube.transform.position = transform.position;
+        }
+
+        cube.MoveDirection = moveDirection;
+        cube.GetComponent<BoxCollider>();
+        cube.GetComponent<Rigidbody>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, cubePrefab.transform.localScale);
+    }
+}
+
+public enum MoveDirection
+{
+    X,
+    Z
+}
---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public float speed;
+    
+    void Update()
+    {
+        speed = 6f;
+        if (Input.GetButtonDown("Fire1"))
+        transform.Translate(Vector3.up * Time.deltaTime * speed, Space.World);
+    }
+}
