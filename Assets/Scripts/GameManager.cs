1d02c6216e69985d82737b106298cd6b4f5154ac
Max <maxmillianrwoods@gmail.com>
2019-03-22 14:30:50 -0700

    Next stable build
    Added a better background and particle system. Tweaked box spawner locations. Made cursor invisible in play mode.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,6 +14,7 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         spawners = FindObjectsOfType<CubeSpawner>();
+        Cursor.visible = false;
     }
     private void Update()
     {
